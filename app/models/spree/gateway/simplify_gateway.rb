cb17238310116a0887be5f303aa46233228dba4b
Lecky Lao <leckylao@gmail.com>
2015-02-01 14:45:23 +1100

    moved all the simplify logic to activemerchant

--- app/models/spree/gateway/simplify_gateway.rb
+++ app/models/spree/gateway/simplify_gateway.rb
@@ -2,54 +2,21 @@ module Spree
   class Gateway::SimplifyGateway < Gateway
     preference :public_key, :string
     preference :private_key, :string
-    require 'simplify'
 
     def provider_class
-      Simplify
+      ActiveMerchant::Billing::SimplifyGateway
     end
 
     def authorize(money, creditcard, gateway_options)
-      p "Money: #{money.inspect}"
-      p "Credit Card: #{creditcard.inspect}"
-      p "Credit Card year: #{creditcard.year.to_s[2..3]}"
-      p "Gateway options: #{gateway_options.inspect}"
-
-      Simplify::Authorization.create({
-        "amount" => money,
-        "description" => "test authorization",
-        "card" => {
-           "expMonth" => creditcard.month,
-           # "expYear" => creditcard.year.to_s[2..3],
-           "expYear" => creditcard.year[2..3],
-           "cvc" => creditcard.verification_value,
-           "number" => creditcard.number
-        },
-        "reference" => "KP-76TBONES",
-        "currency" => gateway_options[:currency]
-      })
+      provider.authorize(money, creditcard, gateway_options)
     end
 
     def purchase(money, creditcard, gateway_options)
-      Simplify::Payment.create({
-        "amount" => money,
-        # "description" => "payment description",
-        # "invoice" => "[INVOICE ID]",
-        "card" => {
-           "expMonth" => creditcard.month,
-           "expYear" => creditcard.year.to_s[2..3],
-           "cvc" => creditcard.verification_value,
-           "number" => creditcard.number
-        }
-      })
+      provider.purchase(money, creditcard, gateway_options)
     end
 
-    def credit(money, creditcard, response_code, gateway_options)
-      Simplify::Refund.create({
-        "amount" => money,
-        # "payment" => "[PAYMENT ID]",
-        # "reason" => "Refund Description",
-        # "reference" => "76398734634"
-      })
+    def refund(money, creditcard, response_code, gateway_options)
+      provider.refund(money, creditcard, response_code, gateway_options)
     end
   end
 end
